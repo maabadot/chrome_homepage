b94c7770a82dfb43a153ba64b7fbe08fe58a1167
Sergey Bolshakov <spooky5ya@yandex.ru>
2020-03-09 23:06:45 +0300

    localStorage parsing, layout

---
+++ bookmarks.js
@@ -0,0 +1,28 @@
+// let bookmarks = {
+//     "categories": [
+//         {
+//             "name": "Social networks",
+//             "categoryBookmarks": [
+//                 {
+//                     "name": "VK",
+//                     "link": "https://vk.com"
+//                 },
+//                 {
+//                     "name": "Insta",
+//                     "link": "https://instagram.com"
+//                 }
+//             ]
+//         },
+//         {
+//             "name": "Programming",
+//             "categoryBookmarks": [
+//                 {
+//                     "name": "localhost:5500",
+//                     "link": "localhost:5500"
+//                 }
+//             ]
+//         }
+//     ]
+// };
+
+// localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const input = document.querySelector('#input');
 
-
-
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         let link = 'https://www.google.com/search?q=' + input.value;
@@ -9,25 +7,75 @@ input.addEventListener("keypress", function(event) {
     }
 })
 
+const addCategory = document.querySelector('.pinned-add-category');
+addCategory.addEventListener('click', function(event) {
+    console.log('clicked');
+});
+
+
+let bookmarksElement = document.querySelector('.bookmarks');
+
+function loadBookmarks() {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks')).categories;
+    for (let i = 0; i < bookmarks.length; i++) {
+        console.log(bookmarks[i]);
+
+        let category = document.createElement('div');
+        category.className += "bookmarks-category";
+
+        let header = document.createElement('p');
+        header.innerHTML = bookmarks[i].name;
+        category.appendChild(header);
+
+        categoryBookmarks = bookmarks[i].categoryBookmarks
+        for (let j = 0; j < categoryBookmarks.length; j++) {
+            bookmark = categoryBookmarks[j];
+
+            let link = document.createElement('a');
+            link.href = bookmark.link;
+            link.innerHTML = bookmark.name;
+
+            let child = document.createElement('div');
+            child.className += 'bookmarks-child';
+            child.appendChild(link);
+
+            category.appendChild(child);
+
+            console.log(bookmark);
+        }
+        console.log(category);
+
+        // let buttonElement = document.createElement('button');
+        // buttonElement.innerHTML = '+';
+        // buttonElement.className += 'button';
+
+        let div = document.createElement('div');
+        div.className += 'bookmarks-button';
+        div.innerHTML = '+';
+        div.id = `button${i}`;
+        // div.appendChild(buttonElement);
+
+        category.appendChild(div);
+
+        bookmarksElement.appendChild(category);
+    }
+    
+}
+
+loadBookmarks()
+
 const buttonAddChild1 = document.querySelector('#button1');
 
 buttonAddChild1.addEventListener('click', function(event) {
 
     let element = document.createElement('div');
-    let p = document.createElement('p');
-    p.innerHTML = 'vk.com';
+    let a = document.createElement('a');
+    a.innerHTML = 'vk.com';
+    a.href = 'https://vk.com';
     element.className += "bookmarks-child"
-    element.append(p);
+    element.append(a);
     
     let category = document.querySelector('.bookmarks-category');
-    // let nodes = document.querySelectorAll('.bookmarks-child');
-    // let button = nodes[nodes.length - 1];
     let button = document.querySelector('.button');
-    console.log(category, element);
     category.insertBefore(element, button);
 })
-
-
-function createElement() {
-
-}
